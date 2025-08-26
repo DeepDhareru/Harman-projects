6f846cbacbc6ff93413bd15727a5804b34c5550b
DeepDhareru <157576862+DeepDhareru@users.noreply.github.com>
2025-08-26 21:05:15 +0530

    Delete Project-1-harman directory

--- Project-1-harman/project-1/script.js
+++
@@ -1,6 +0,0 @@
-const textarea = document.getElementById("textInput");
-const charCount = document.getElementById("charCount");
-
-textarea.addEventListener("input", () => {
-  charCount.textContent = textarea.value.length;
-});
